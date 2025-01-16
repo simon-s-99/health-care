c248264056e21a1675fc142ddf364639b03e117b
Gabriel Wedin <142593473+gabrielwedin@users.noreply.github.com>
2025-01-16 11:08:37 +0100

    Added new fields to User and RegisterDto (#25)
    * Added new fields to user and registerDto
    
    * Added required fields for User and DTO models
    
    Also added check in Controller that email och phonenumber doesnt already exist.
    
    * Update AuthController.cs

--- HealthCareABApi/HealthCareABApi/DTO/LoginDto.cs
+++ HealthCareABApi/HealthCareABApi/DTO/LoginDto.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 namespace HealthCareABApi.DTO
 {
     public class LoginDto
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
