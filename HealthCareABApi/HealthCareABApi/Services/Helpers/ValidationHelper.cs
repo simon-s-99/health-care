6bab4249889aa0f899b17b73e92e086f0e5ba06d
Sam <zaaamuel_96@hotmail.com>
2025-01-26 19:26:43 +0100

    Added ValidationHelper.cs

---
+++ HealthCareABApi/HealthCareABApi/Services/Helpers/ValidationHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace HealthCareABApi.Services.Helpers
+{
+    public class ValidationHelper
+    {
+        public static bool HasComplexity(string password)
+        {
+            return password.Any(char.IsUpper) &&
+                   password.Any(char.IsLower) &&
+                   password.Any(char.IsDigit) &&
+                   password.Any(ch => !char.IsLetterOrDigit(ch)); // Special character check
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            var emailRegex = @"^[^@\s]+@[^@\s]+\.[^@\s]+$"; // Basic email format validation. Not pretty but should work!
+            return Regex.IsMatch(email, emailRegex, RegexOptions.IgnoreCase);
+        }
+
+        public static bool IsValidPhoneNumber(string phoneNumber)
+        {
+            var phoneRegex = @"^\+?[1-9]\d{1,14}$"; // E.164 format validation
+            return Regex.IsMatch(phoneNumber, phoneRegex);
+        }
+    }
+}
