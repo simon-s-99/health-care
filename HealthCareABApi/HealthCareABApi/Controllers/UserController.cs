de9ec4adc824e6dfe46a1bf1c9e6e9d25707662a
Adam Kumlin <skola.adamkumlin99@gmail.com>
2025-01-30 15:43:12 +0100

    feat: Add authorization to getUser

--- HealthCareABApi/HealthCareABApi/Controllers/UserController.cs
+++ HealthCareABApi/HealthCareABApi/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using HealthCareABApi.Services.Implementations;
 using HealthCareABApi.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace HealthCareABApi.Controllers
 {
@@ -24,19 +25,49 @@ namespace HealthCareABApi.Controllers
         {
             try
             {
-                var user = await _userService.GetUserByIdAsync(id);
+                // Get the logged in users role and identifier
+                var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                var roles = User.FindFirst(ClaimTypes.Role)?.Value;
 
-                GetUserDTO dto = new GetUserDTO
+                // If a user (patient) want to access a user
+                if (!roles.Contains("Admin"))
                 {
-                    Firstname = user.Firstname,
-                    Lastname = user.Lastname,
-                    Email = user.Email,
-                    Phonenumber = user.Phonenumber,
-                    Username = user.Username,
-                    Roles = user.Roles
-                };
-                return Ok(dto);
+                    var userUser = await _userService.GetUserByIdAsync(id);
 
+                    if (userId != id)
+                    {
+                        // If the accessed user is not themselves or a caregiver, return forbidden
+                        if (!userUser.Roles.Contains("Admin"))
+                        {
+                            return Forbid("You do not have access to this user.");
+                        }
+
+                        GetUserDTO userDto = new GetUserDTO
+                        {
+                            Firstname = userUser.Firstname,
+                            Lastname = userUser.Lastname,
+                            Email = userUser.Email,
+                            Phonenumber = userUser.Phonenumber,
+                            Username = userUser.Username,
+                            Roles = userUser.Roles
+                        };
+                        return Ok(userDto);
+                    }
+                }
+
+                // If admin return user
+                var adminUser = await _userService.GetUserByIdAsync(id);
+
+                GetUserDTO adminDto = new GetUserDTO
+                {
+                    Firstname = adminUser.Firstname,
+                    Lastname = adminUser.Lastname,
+                    Email = adminUser.Email,
+                    Phonenumber = adminUser.Phonenumber,
+                    Username = adminUser.Username,
+                    Roles = adminUser.Roles
+                };
+                return Ok(adminDto);
             }
             catch (Exception ex)
             {
